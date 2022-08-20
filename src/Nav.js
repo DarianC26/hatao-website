0db2611c5377fcf0e5f1a5465f86724b368cbc34
DarianC26 <dariancheung@gmail.com>
2022-08-19 23:20:40 -0400

    nav bar created

---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Nav.css';
+import logo from './images/Ha-Tao.png';
+import {Link} from 'react-router-dom';
+
+function Nav() {
+    return (
+        <nav>
+            <img src={logo} alt=''></img>
+            <ul className='nav-links'>
+                <Link to="/">
+                    <li>HOME</li>
+                </Link>
+                <Link to="/contact">
+                    <li>CONTACT</li>
+                </Link>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
