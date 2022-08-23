513d00697e1239590559ebfeae78849ecfd97fe4
DarianC26 <dariancheung@gmail.com>
2022-08-23 17:12:24 -0400

    reformatting + location div created

--- src/Home.js
+++ src/Home.js
@@ -69,6 +69,10 @@ function Home() {
                     </div>
                 </div>
             </div>
+
+            <div className='location'>
+
+            </div>
         </div>
     );
 }
